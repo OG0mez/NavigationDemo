b1528db8ff841391325d8ed84f3c1b77639721fc
Oscar <oscar.a_gh@hotmail.com>
2019-01-28 18:49:17 -0600

    created 3 example fragmetns (Account , Seetings , Name)

--- app/src/main/java/com/oscar/navigationdemo/AccountFragment.kt
+++ app/src/main/java/com/oscar/navigationdemo/AccountFragment.kt
@@ -7,23 +7,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
-import kotlinx.android.synthetic.main.fragment_main.*
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_account.*
 
 
-class MainFragment : Fragment() {
+class AccountFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_account, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        goToAccountBtn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.toAccount))
-        goToSettingBtn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.toSettings))
+        nextBtn.setOnClickListener {
+            val nameBundle = Bundle()
+            nameBundle.putString("nameArg",this.NameText.text.toString())
+            it.findNavController().navigate(R.id.nameFragment,nameBundle)
+        }
     }
 
 
--- app/src/main/java/com/oscar/navigationdemo/NameFragment.kt
+++ app/src/main/java/com/oscar/navigationdemo/NameFragment.kt
@@ -6,24 +6,22 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.fragment_name.*
 
 
-class MainFragment : Fragment() {
+class NameFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_name, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        goToAccountBtn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.toAccount))
-        goToSettingBtn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.toSettings))
+        textNameView.text = arguments?.getString("nameArg")
     }
 
 
